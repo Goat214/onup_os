73b4366413de641e6ac442e0c80004780908b6df
Abdulatif <abdulatif@gmail.com>
2026-03-02 15:57:32 +0600

    navbar end

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,34 +1,55 @@
 import { Link } from "react-router-dom";
 import logo from "../images/logo_O.jpg";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="navbar">
       <div className="wrap">
-        <Link to="/" className="nb-logo">
-          <img src={logo} alt="Onup OS" />
-          <span className="nb-logo-text">ONUP OS</span>
-        </Link>
+        <a href="/" className="nb-logo">
+          <img src={logo} alt="logo" />
+          <span className="nb-logo-text">Onup OS</span>
+        </a>
 
-        <ul className="nb-links">
-          <li><Link to="/courses">Курстар</Link></li>
-          <li><a href="#about">Биз жөнүндө</a></li>
-          <li><a href="#directions">Багыттар</a></li>
-          <li><a href="#location">Дарек</a></li>
+        <ul className={`nb-links ${menuOpen ? "open" : ""}`}>
+          <li>
+            <a href="#courses" onClick={() => setMenuOpen(false)}>
+              Курстар
+            </a>
+          </li>
+          <li>
+            <a href="#about" onClick={() => setMenuOpen(false)}>
+              Биз жөнүндө
+            </a>
+          </li>
+          <li>
+            <a href="#directions" onClick={() => setMenuOpen(false)}>
+              Багыттар
+            </a>
+          </li>
+          <li>
+            <a href="#contact" onClick={() => setMenuOpen(false)}>
+              Дарек
+            </a>
+          </li>
         </ul>
 
         <div className="nb-right">
-          <a
-            href="https://t.me/+WEiuzpYRk4BlNzli"
-            className="nb-team"
-            target="_blank"
-            rel="noreferrer"
+          <a href="#team" className="nb-team">
+            Команда
+          </a>
+          <a href="#enroll" className="nb-cta">
+            Кабыл алуу
+          </a>
+          <button
+            className={`nb-burger ${menuOpen ? "open" : ""}`}
+            onClick={() => setMenuOpen(!menuOpen)}
           >
-            Биздин команда
-          </a>
-          <a href="#consult1" className="nb-cta">
-            КОНСУЛЬТАЦИЯ
-          </a>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
         </div>
       </div>
     </nav>
